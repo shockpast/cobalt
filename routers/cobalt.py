9231de6ad4cf3b2d4f315ce0c445ba6a4f89247b
shockpast <shockpast@gmail.com>
2025-07-13 19:51:44 +0300

    feat: added inline mode

--- routers/cobalt.py
+++ routers/cobalt.py
@@ -1,12 +1,14 @@
 from urllib.parse import urlparse
 import pathlib
+import uuid
 
 from aiogram import Router, F, types, filters
 import validators
 
-from services.database import Database
 from i18n import i18n
+from services.database import Database
 from services.cobalt import Error, PickerResponse, TunnelRedirectResponse, download
+import utilities
 
 router = Router()
 
@@ -58,6 +60,48 @@ async def _download(message: types.Message):
 
   if len(media_group) >= 1:
     return await message.reply_media_group(media_group)
+
+@router.inline_query()
+async def download_from_inline(query: types.InlineQuery):
+  mime_type = { 
+    ".opus": "audio/opus", ".mp3": "audio/mpeg", ".ogg": "audio/ogg",
+    ".mp4": "video/mp4", ".webm": "video/webm"
+  }
+
+  url = query.query.strip()
+  if not "https://" in url:
+    return
+
+  t = i18n(query.from_user.language_code)
+
+  response = await download(query.from_user.id, url)
+  results = []
+
+  if isinstance(response, Error):
+    await query.answer([], switch_pm_text=t.get(response.code), switch_pm_parameter="error")
+    return
+
+  if isinstance(response, PickerResponse):
+    for item in response.picker:
+      if item.type == "photo":
+        results.append(types.InlineQueryResultPhoto(id=str(uuid.uuid4()), photo_url=item.url, title=url, thumbnail_url=item.thumb))
+      if item.type == "gif":
+        results.append(types.InlineQueryResultGif(id=str(uuid.uuid4()), gif_url=item.url, title=url, thumbnail_url=item.thumb))
+      if item.type == "video":
+        results.append(types.InlineQueryResultVideo(id=str(uuid.uuid4()), video_url=item.url, title=url, thumbnail_url=item.thumb))
+  if isinstance(response, TunnelRedirectResponse):
+    media_type = pathlib.Path(response.filename).suffix
+
+    if media_type in (".opus", ".mp3", ".ogg"):
+      results.append(types.InlineQueryResultAudio(id=str(uuid.uuid4()), audio_url=response.url, title=url, mime_type=mime_type.get(media_type), thumbnail_url=response.url))
+    if media_type == ".gif":
+      results.append(types.InlineQueryResultGif(id=str(uuid.uuid4()), gif_url=response.url, title=url, thumbnail_url=response.url))
+    if media_type in (".mp4", ".webm"):
+      results.append(types.InlineQueryResultVideo(id=str(uuid.uuid4()), video_url=response.url, title=url, mime_type=mime_type.get(media_type), thumbnail_url=response.url))
+    if media_type in (".png", ".jpg", ".jpeg", ".webp"):
+      results.append(types.InlineQueryResultPhoto(id=str(uuid.uuid4()), photo_url=response.url, title=url, thumbnail_url=response.url))
+
+  await query.answer(results, cache_time=1)
 
 @router.message(F.text.startswith("https://") | F.text.startswith("http://"))
 async def download_from_text(message: types.Message):
